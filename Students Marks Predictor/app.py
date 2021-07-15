26c90638ac07b1cac00c3b2950a21f1fa56b72d7
Rituj Srivastava <rituj1103@gmail.com>
2021-07-15 18:16:21 +0530

    Update app.py

--- Students Marks Predictor/app.py
+++ Students Marks Predictor/app.py
@@ -37,6 +37,6 @@ def predict():
     return render_template('index.html', prediction_text='You will get [{}%] marks, when you do study [{}] hours per day '.format(output, int(features_value[0])))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+#if __name__ == "__main__":
+ #   app.run(host='0.0.0.0', port=8080)
     
